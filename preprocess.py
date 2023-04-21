fb08c660c21991993a6c5ec13305f5a53fb17689
Xsmos <xiabin.phy@gmail.com>
2023-04-20 22:59:09 -0400

    Now preprocess.py can preprocess test/

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,49 @@ import h5py
 
 dt = h5py.special_dtype(vlen=str)
 
+def subtract_both(dataset, which="test", debug=False):
+    dataset = str(dataset)
+    inputDir = "./seti-breakthrough-listen"
+    dataDir = os.path.join(inputDir, which, dataset)
+
+    t_start = time()
+    id_list = os.listdir(dataDir)
+
+    for idx in id_list:
+        x = np.load(os.path.join(dataDir, idx))
+        off_averaged = np.average(x[1::2], axis=0)
+        on_substracted = x[0::2] - off_averaged
+
+        if "all_figures_in_dataset" in vars():
+            all_figures_in_dataset = np.vstack((all_figures_in_dataset, on_substracted.reshape(
+                1, on_substracted.shape[0], on_substracted.shape[1], on_substracted.shape[2])))
+            # labels_in_dataset = np.append(labels_in_dataset, int(train_labels.target.iloc[i]))
+            ids_of_labels = np.append(ids_of_labels, idx[-4:])
+            print("{}/{}".format(all_figures_in_dataset.shape[0], len(id_list)), end="\r")
+        else:
+            print("Preprocessing {}/...".format(dataset))
+            all_figures_in_dataset = np.array([on_substracted])
+            # labels_in_dataset = int(train_labels.target.iloc[i])
+            ids_of_labels = idx[-4:]
+
+        if debug and all_figures_in_dataset.shape[0] >= 100:
+            break
+
+    if not os.path.exists(os.path.join(inputDir, which+"_preprocessed")):
+        os.makedirs(os.path.join(inputDir, which+"_preprocessed"))
+
+    with h5py.File(os.path.join(inputDir, which+'_preprocessed', "{}.h5".format(dataset)), 'w') as hf:
+        # print("\ntarget.shape = ", labels_in_dataset.shape)
+        # hf.create_dataset("target", data=labels_in_dataset)
+        print("figure.shape = ", all_figures_in_dataset.shape)
+        hf.create_dataset("figure", data=all_figures_in_dataset)
+        print("id.shape =", ids_of_labels.shape)
+        hf.create_dataset("id", dtype=dt, data=ids_of_labels.tolist())
+
+    t_end = time()
+    total_time = t_end - t_start
+    print("It costs {:.3f} s to preprocess the dataset.".format((total_time)))
+
 
 def subtract(dataset, debug=False):
     """
@@ -62,13 +105,13 @@ def subtract(dataset, debug=False):
     print("It costs {:.3f} s to preprocess the dataset.".format((total_time)))
 
 
-def combine(h5sdir="train", all=False, debug=False):
+def combine(which="test", all=False, debug=False):
     """
-    Combine every subtracted .h5 files in h5sdir into single one .h5 file.
+    Combine every subtracted .h5 files in which into single one .h5 file.
     If all=True, include files that were not downloaded yet and subtract them to get corresponding .h5s.
     """
 
-    dir = os.path.join("./seti-breakthrough-listen", h5sdir+"_preprocessed")
+    dir = os.path.join("./seti-breakthrough-listen", which+"_preprocessed")
 
     if all:
         file_list = np.append(np.arange(10).astype(
@@ -81,12 +124,17 @@ def combine(h5sdir="train", all=False, debug=False):
             print("{}.h5 exists, skipping.".format(i))
             continue
         else:
-            subtract(i, debug=debug)
+            if which == 'train':
+                subtract(i, debug=debug)
+            elif which == "test":
+                subtract_both(i, which, debug)
+            else:
+                print("'which' should be one of 'train' or 'test'.")
             print("---"*30)
 
-    save_dir = os.path.join(dir, h5sdir+".h5")
+    save_dir = os.path.join(dir, which+".h5")
     if os.path.exists(save_dir):
-        print(h5sdir+".h5 already exists, skipping.")
+        print(which+".h5 already exists, skipping.")
     else:
         with h5py.File(save_dir, 'a') as file:
             for name in file_list:
@@ -96,21 +144,21 @@ def combine(h5sdir="train", all=False, debug=False):
                     if name == '0':
                         figure = file.create_dataset(
                             'figure', data=f['figure'], maxshape=(None,None,None,None))
-                        target = file.create_dataset(
-                            "target", data=f['target'], maxshape=(None,))
                         idx = file.create_dataset(
                             "id", data=f['id'], maxshape=(None, ))
+                        if which == 'train':
+                            target = file.create_dataset("target", data=f['target'], maxshape=(None,))
                     else:
                         figure.resize(
                             figure.shape[0]+f['figure'].shape[0], axis=0)
                         figure[-f['figure'].shape[0]:] = f['figure']
-                        target.resize(
-                            target.shape[0]+f['target'].shape[0], axis=0)
-                        target[-f['target'].shape[0]:] = f['target']
                         idx.resize(idx.shape[0]+f['id'].shape[0], axis=0)
                         idx[-f['id'].shape[0]:] = f['id']
+                        if which == "train":
+                            target.resize(target.shape[0]+f['target'].shape[0], axis=0)
+                            target[-f['target'].shape[0]:] = f['target']
 
 
 if __name__ == "__main__":
     # subtract(1)
-    combine("train", all=True, debug=False)
+    combine("test", all=False, debug=True)
